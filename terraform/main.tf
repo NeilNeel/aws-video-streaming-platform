57c5efd000b8723f175c432971444b8b71b5e019
NeilNeel <neelbarvaliya04@gmail.com>
2025-08-29 15:07:39 -0400

    change the IP address from security group to my new IP

--- terraform/main.tf
+++ terraform/main.tf
@@ -78,7 +78,7 @@ resource "aws_security_group" "web_sg" {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = ["98.109.109.51/32"]
+        cidr_blocks = ["73.199.9.136"]
     }
 
     ingress {
